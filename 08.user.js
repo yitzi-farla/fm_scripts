db8e5c380ef885c5111bc4eee65137dfa2ee4cf7
yitzi-farla <yitzi.farla@gmail.com>
2026-06-04 06:33:55 +0300

    Add numeric slot 08

---
+++ 08.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Farla 08
+// @namespace    farla-office-scripts
+// @version      0.1.0
+// @description  Reserved Farla script slot 08
+// @match        https://farla2.tradepeg.net/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/08.user.js
+// @downloadURL  https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/08.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  console.log('Farla 08 reserved script slot');
+})();
