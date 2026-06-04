1fc692aacf935e11bed8a1578d206df4666305dd
yitzi-farla <yitzi.farla@gmail.com>
2026-06-04 06:32:01 +0300

    Add numeric placeholder 04

---
+++ 04.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Farla 04
+// @namespace    farla-office-scripts
+// @version      0.1.0
+// @description  Placeholder Tampermonkey file
+// @match        https://farla2.tradepeg.net/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/04.user.js
+// @downloadURL  https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/04.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+})();
