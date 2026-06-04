d7372a083ba479241e4004ab25ca82aea4138be9
yitzi-farla <yitzi.farla@gmail.com>
2026-06-04 06:34:29 +0300

    Add numeric slot 09

---
+++ 09.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Farla 09
+// @namespace    farla-office-scripts
+// @version      0.1.0
+// @description  Reserved Farla script slot 09
+// @match        https://farla2.tradepeg.net/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/09.user.js
+// @downloadURL  https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/09.user.js
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  console.log('Farla 09 reserved script slot');
+})();
