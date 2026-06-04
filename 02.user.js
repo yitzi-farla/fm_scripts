425c8d58a0cb2e200467696a42b0bfadfc3e97a6
yitzi-farla <yitzi.farla@gmail.com>
2026-06-04 06:43:55 +0300

    Update 02.user.js

--- 02.user.js
+++ 02.user.js
@@ -1,11 +1,12 @@
-// ==UserScript==
-// @name         TradePeg Product Sales Drilldown SO/PO Tabs
+// @name         Farla 02 - TradePeg Product Sales Drilldown SO/PO Tabs
 // @namespace    farla-tradepeg
-// @version      1.7
+// @version      1.7.2
 // @description  Add product-specific Sales Order and Purchase/Receipt tabs to product detail pages with CSV export, sortable columns, and enriched RCT/PO rows
 // @match        https://farla2.tradepeg.net/wapp/en-gb/inventory/products/details/*
 // @match        https://farla2.tradepeg.net/app/en-gb/inventory/products/details/*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/02.user.js
+// @downloadURL  https://raw.githubusercontent.com/yitzi-farla/fm_scripts/main/02.user.js
 // ==/UserScript==
 
 (function () {
